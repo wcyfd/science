4a74b58c6293af47bc4f9b5eabebf6a4d0b27986
wcy <1101697691@qq.com>
2019-12-27 17:43:20 +0800

    gengxin

--- src/main/java/com/science/game/App.java
+++ src/main/java/com/science/game/App.java
@@ -1,12 +1,7 @@
 package com.science.game;
 
-import com.science.game.service.Service;
-import com.science.game.service.item.ItemService;
-import com.science.game.service.item.ItemServiceImpl;
-import com.science.game.service.nature.NatureService;
-import com.science.game.service.nature.NatureServiceImpl;
-import com.science.game.service.village.VillageService;
-import com.science.game.service.village.VillageServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import game.quick.window.GameWindows;
 
@@ -15,28 +10,21 @@ import game.quick.window.GameWindows;
  *
  */
 public class App {
-	public static GameWindows win = new GameWindows();
+	public static GameWindows win = null;
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-		Service.regist(ItemServiceImpl.class, ItemService.class);
-		Service.regist(VillageServiceImpl.class, VillageService.class);
-		Service.regist(NatureServiceImpl.class, NatureService.class);
-		Service.initServices();
-
-		win = new GameWindows();
-		win.setHandler(new ScienceHandler());
-		win.setView(new ScienceView());
-
-		win.start();
-		win.startConsoleThread();
-
-		GameWindows cmdWin = new GameWindows();
-		cmdWin.setView(new CommandView());
-		cmdWin.start();
-	}
-
-	public static void render() {
-		win.render();
+//		Service.regist(ItemServiceImpl.class, ItemService.class);
+//		Service.regist(VillageServiceImpl.class, VillageService.class);
+//		Service.regist(NatureServiceImpl.class, NatureService.class);
+//		Service.initServices();
+//
+//		win = GameWindows.Builder.create().setHandler(new ScienceHandler()).setView(new ScienceView()).build()
+//				.startConsoleThread();
+//
+//		GameWindows.Builder.create().setView(new CommandView()).build();
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		
 	}
 
 }
